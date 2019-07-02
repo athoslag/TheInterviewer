c7f96848c5cdb445af35f4087827246fb99d2afe
athoslag <athoslagemann@gmail.com>
2019-07-02 19:38:58 -0300

    Add MainViewModel, update file hierarchy

---
+++ TheInterviewer/ViewModels/MainViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  MainViewModel.swift
+//  TheInterviewer
+//
+//  Created by Athos Lagemann on 02/07/19.
+//  Copyright © 2019 Athos Lagemann. All rights reserved.
+//
+
+import Foundation
+
+final class MainViewModel {
+    init() {
+        // Code
+    }
+}
