e4957b0db2c19754611fa0ba4759c31e962bceb3
athoslag <athoslagemann@gmail.com>
2019-09-11 16:55:54 -0300

    Implement Final Screen

--- TheInterviewer/Controller/Interview/FinalScreenViewController.swift
+++ TheInterviewer/Controller/Interview/FinalScreenViewController.swift
@@ -8,15 +8,21 @@
 
 import UIKit
 
-final class FinalScreenViewController: UIViewController {
+protocol FinalScreenDelegate: class {
+    func didTapShare(_ viewController: FinalScreenViewController)
+    func didTapSave(_ viewController: FinalScreenViewController)
+    func didTapDiscard(_ viewController: FinalScreenViewController)
+}
 
+final class FinalScreenViewController: UIViewController {
     @IBOutlet private weak var titleLabel: UILabel!
     @IBOutlet private weak var tableView: UITableView!
     @IBOutlet private weak var shareButton: UIButton!
     @IBOutlet private weak var saveButton: UIButton!
-    @IBOutlet private weak var exitButton: UIButton!
+    @IBOutlet private weak var discardButton: UIButton!
     
-    let viewModel: InterviewViewModel
+    private let viewModel: InterviewViewModel
+    weak var delegate: FinalScreenDelegate?
     
     init(viewModel: InterviewViewModel) {
         self.viewModel = viewModel
@@ -36,32 +42,68 @@ final class FinalScreenViewController: UIViewController {
     }
     
     private func setupUI() {
+        // Title
+        titleLabel.font = UIFont(SFPro: .display, variant: .medium, size: 24)
         
+        // TableView
+        tableView.tableFooterView = UIView()
+        
+        // Share
+        shareButton.setTitleColor(.white, for: .normal)
+        shareButton.backgroundColor = AppConfiguration.mainColor
+        shareButton.layer.cornerRadius = shareButton.layer.bounds.height / 2
+        shareButton.titleLabel?.font = UIFont(SFPro: .text, variant: .regular, size: 18)
+        
+        // Save
+        saveButton.setTitleColor(.blue, for: .normal)
+        saveButton.layer.cornerRadius = saveButton.layer.bounds.height / 2
+        saveButton.titleLabel?.font = UIFont(SFPro: .text, variant: .regular, size: 18)
+        
+        // Exit
+        discardButton.setTitleColor(.red, for: .normal)
+        discardButton.layer.cornerRadius = discardButton.layer.bounds.height / 2
+        discardButton.titleLabel?.font = UIFont(SFPro: .text, variant: .regular, size: 18)
     }
 }
 
 // MARK - Actions
 extension FinalScreenViewController {
     @IBAction private func didTapShare(_ sender: UIButton) {
-    
+        delegate?.didTapShare(self)
     }
     
     @IBAction private func didTapSave(_ sender: UIButton) {
-    
+        delegate?.didTapSave(self)
     }
     
-    @IBAction private func didTapExit(_ sender: UIButton) {
-    
+    @IBAction private func didTapDiscard(_ sender: UIButton) {
+        delegate?.didTapDiscard(self)
     }
 }
 
 // MARK - TableView
 extension FinalScreenViewController: UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return viewModel.numberOfParts
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return viewModel.numberOfSections(part: section)
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return viewModel.partTitle(part: section)
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let sectionTitle = viewModel.sectionTitle(part: indexPath.section, section: indexPath.row)
+        let total = viewModel.sections[indexPath.row].questionPairs.count
+        let answered = viewModel.sections[indexPath.row].questionPairs.filter{ $0.answer != nil }.count
+        let sectionDetail = "\((answered / total) * 100)% respondida"
+        
+        let cell = UITableViewCell(style: .subtitle, reuseIdentifier: "FinalScreen")
+        cell.textLabel?.text = sectionTitle
+        cell.detailTextLabel?.text = sectionDetail
         return UITableViewCell()
     }
 }
