087f7a5de79e36558ddbf9884c6a1e0a58c85c76
athoslag <athoslagemann@gmail.com>
2019-10-01 20:12:05 -0300

    Add zip archive to share options

--- TheInterviewer/Coordinator/InterviewCoordinator.swift
+++ TheInterviewer/Coordinator/InterviewCoordinator.swift
@@ -180,7 +180,11 @@ extension InterviewCoordinator: QALongViewControllerDelegate {
 extension InterviewCoordinator: FinalScreenDelegate {
     func didTapShare(_ viewController: FinalScreenViewController) {
         guard let interview = viewModel.shareInterview() else { return }
-        let items: [Any] = [interview]
+        var items: [Any] = [interview]
+        
+        if let url = ZipfileService.zipFilesAt(path: viewModel.interviewDirectory, filename: viewModel.title) {
+            items.append(url)
+        }
         
         let shareViewController = UIActivityViewController(activityItems: items, applicationActivities: nil)
         shareViewController.popoverPresentationController?.sourceView = viewController.view
