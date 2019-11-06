fd09372e5fafb520e61559a0d526e55692ecb630
athoslag <athoslagemann@gmail.com>
2019-11-05 21:26:42 -0300

    Disable idle timer, update build number

--- TheInterviewer/Configuration/AppDelegate.swift
+++ TheInterviewer/Configuration/AppDelegate.swift
@@ -18,6 +18,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         window = UIWindow(frame: UIScreen.main.bounds)
         
+        UIApplication.shared.isIdleTimerDisabled = true
+        
         coordinator = AppCoordinator(window: window!)
         coordinator.start()
         
